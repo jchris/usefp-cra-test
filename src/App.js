45faf3102def6e30c67dfc15c344b7f393f4dd84
Chris Anderson <jchris@gmail.com>
2024-07-16 13:26:34 -0700

    allDocs crashes less but still doesn't work right

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import { useFireproof, useLiveQuery } from 'use-fireproof';
+import { useFireproof, useAllDocs } from 'use-fireproof';
 
 function App() {
 
-  const { useDocument } = useFireproof('xyz');
+  const { useDocument, database } = useFireproof('xyz');
 
   const [doc, setDoc, saveDoc] = useDocument(() => ({ message: 'Hello World', created: Date.now(), updated: Date.now() }));
 
-  // const allDocs = useLiveQuery('_id')
+  const allDocs = useAllDocs()
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setDoc({ ...doc, updated: Date.now() });
+    database.put({ ...doc, updated: Date.now() });
     saveDoc();
   };
 
   const handleChange = (event) => {
     setDoc({ ...doc, message: event.target.value });
   };
+
+  console.log(allDocs)
 
   return (
     <div className="App">
@@ -32,12 +33,12 @@ function App() {
           <input type="text" value={doc.message} onChange={handleChange} />
           <button type="submit">Save</button>
         </form>
+        <ul>
+          {allDocs.docs.map((doc) => (
+            <li key={doc._id}>{doc.message}</li>
+          ))}
+        </ul>
       </header>
-      <ul>
-        {/* {allDocs.docs.map((doc) => (
-          <li key={doc._id}>{doc.message}</li>
-        ))} */}
-      </ul>
     </div>
   );
 }
